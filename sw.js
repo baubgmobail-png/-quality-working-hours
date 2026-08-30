f1d7309391df1a62e1070600e5d4a524d12869e0
baubgmobail-png <baubgmobail@gmail.com>
2026-08-30 04:58:59 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-const CACHE='qwh-v2';const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./cover.jpeg','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
